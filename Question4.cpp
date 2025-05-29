0aa02a619ba8ed9f1623195830d69fd7d8de97ae
rishita2305 <rishita.chaudhary2020@gmail.com>
2025-05-29 13:26:52 +0530

    Create Question4.cpp

---
+++ Question4.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/problemset/problem/1899/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        if(n%3==0){
+            cout<< "Second" <<endl;
+        }
+        else{
+            cout<< "First" << endl;
+        }
+}}
+//total tc = O(t)
+// total sc = O(1)
